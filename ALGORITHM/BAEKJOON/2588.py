d465fe522ab58680ab04bcef23182d565188f4e2
Kwan-Gu, Baek <bouguereau1130@gmail.com>
2020-07-27 03:32:52 +0900

    Create 2588.py
    백준 2588

---
+++ ALGORITHM/BAEKJOON/2588.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 03:15:36 2020
+
+@author: bkk11
+"""
+
+a = input()
+b = input()
+
+print(int(a) * int(b[2]))
+print(int(a) * int(b[1]))
+print(int(a) * int(b[0]))
+
+print(int(a) * int(b))
