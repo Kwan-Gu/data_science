0cf330c7f35448b8df2a1080cb46c7f0ac305013
Kwan-Gu, Baek <bouguereau1130@gmail.com>
2020-09-15 00:24:29 +0900

    Create .gitignore

--- ALGORITHM/BAEKJOON/1038.py
+++ ALGORITHM/BAEKJOON/1038.py
@@ -5,4 +5,20 @@ Created on Fri Sep  4 00:08:56 2020
 @author: WHITE
 """
 
+#n = int(input())
+
+table = [[1]*10]
+for j in range(9): # 0, 8
+    temp = [0]*(j+1)
+    for i in range(j, 9):
+        temp += [temp[-1]+table[j][i]]
+    table.append(temp)
+
 n = int(input())
+sum = 0
+for j in range(len(table)):
+    for i in range(j,len(table[0])):
+        sum+=table[j][i]
+        if n>=sum:
+            a, b = j, i
+            break
